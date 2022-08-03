85a3b0b96de3d8ad8284bc3d34d9b0544d0e4a74
LontchiFrank <fodjof6@gmail.com>
2022-08-03 11:18:05 +0100

    slide animation

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,23 +1,174 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Signup.css";
 
 function Signup() {
+  const [formData, setFormData] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    tel: "",
+    password: "",
+  });
+  const { fname, lname, email, tel, password } = formData;
+
+  const [isOpen, setIsOpen] = useState(false);
+  const myElement = useRef(null);
+  const first = useRef(null);
+  const second = useRef(null);
+  const third = useRef(null);
+
+  const handleClick = () => {
+    first.current.style.transform = "translateX(700px)";
+    first.current.style.transition = "3s";
+    first.current.style.zIndex = "3";
+    second.current.style.transform = "translateX(-450px)";
+    second.current.style.transitionDelay = "1s";
+    second.current.style.transition = "3s";
+    second.current.style.transitionTimingFunction = "linear";
+    setIsOpen(true);
+  };
+
+  const handleSecondClick = () => {
+    first.current.style.transform = "translateX(0px)";
+    first.current.style.transition = "3s";
+    first.current.style.zIndex = "3";
+    second.current.style.transform = "translateX(0px)";
+    // second.current.style.transitionDelay = "1s";
+    second.current.style.transition = "3s";
+    second.current.style.transitionTimingFunction = "linear";
+
+    setIsOpen(false);
+    console.log(isOpen);
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="main">
       <div className="container-middle">
-        <div className="firstpart">
+        <div className="firstpart" ref={first}>
           <div className="overlay">
             <div className="head">
               <h3>Digital</h3>
             </div>
             <div className="title">
               <h1>
-                Artificial Intelligence Driving Results for the Tranvel Industry
+                Artificial Intelligence Driving Results for the Travel Industry
               </h1>
+            </div>
+            <div className="bodies">
+              <p>
+                Discover the world's best Travel Industries using Artificial
+                Intelligence
+              </p>
+            </div>
+            <div className="foot">
+              <p>
+                Simply Unbelievable satisfied with my projects and business.! If
+                you already have an account Login.
+              </p>
+
+              {isOpen ? (
+                <div className="bton">
+                  <button onClick={() => handleSecondClick()}>Register</button>
+                </div>
+              ) : (
+                <div className="bton">
+                  <button onClick={() => handleClick()}>Login</button>
+                </div>
+              )}
             </div>
           </div>
         </div>
-        <div className="secondpart"></div>
+        {isOpen ? (
+          <div className="secondpart" ref={second}>
+            <div className="title-head">
+              <h2>Sign In</h2>
+            </div>
+            <div className="form-part">
+              <form className="forms">
+                <div className="lay1">
+                  <label>Email:</label>
+                  <input type="email" placeholder="Enter Email" />
+                </div>
+                <div className="lay1">
+                  <label>Password:</label>
+                  <input type="password" placeholder="Enter Password" />
+                </div>
+                <div className="bton1">
+                  <button>Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        ) : (
+          <div className="secondpart" ref={second}>
+            <div className="title-head">
+              <h2>Sign Up</h2>
+            </div>
+            <div className="form-part">
+              <form className="forms">
+                <div className="lay1">
+                  <label>First Name:</label>
+                  <input
+                    name="fname"
+                    type="text"
+                    value={fname}
+                    placeholder="Enter First Name"
+                  />
+                </div>
+                <div className="lay1">
+                  <label>Last Name:</label>
+                  <input
+                    name="lname"
+                    type="text"
+                    value={lname}
+                    placeholder="Enter Last Name"
+                  />
+                </div>
+                <div className="lay1">
+                  <label>Email:</label>
+                  <input
+                    name="email"
+                    type="email"
+                    value={email}
+                    placeholder="Enter Email"
+                  />
+                </div>
+                <div className="lay1">
+                  <label>Telephone Number:</label>
+                  <input
+                    name="tel"
+                    type="tel"
+                    value={tel}
+                    placeholder="Enter Telephone Number"
+                  />
+                </div>
+                <div className="lay1">
+                  <label>Password:</label>
+                  <input
+                    name="password"
+                    type="password"
+                    value={password}
+                    placeholder="Enter Password"
+                  />
+                </div>
+                <div className="lay2">
+                  <label>Upload Image</label>
+                  <div className="files_bton">
+                    <input type="file" id="file" name="img" accept="image/*" />
+                    <label for="file">Choose File</label>
+                  </div>
+                </div>
+                <div className="bton1">
+                  <button>Create Account</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
